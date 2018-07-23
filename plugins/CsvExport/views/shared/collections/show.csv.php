e1f5cc969d1cdb62b7d08b60af1f26c3c6d21062
Arden Kirkland <arden@ardenkirkland.com>
2018-07-22 18:31:20 -0700

    deleted CsvExport to make room to import through git as submodule

--- plugins/CsvExport/views/shared/collections/show.csv.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Recursively find all items and print CSV of them
-printCsvExport(CsvExport_CollectionAttachUtil::getSubitems($collection));
--- plugins/CsvExport/views/shared/items/show.csv.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Render CSV of the item and its associated annotations
-printCsvExport(CsvExport_ItemAttachUtil::getThisAndAnnotations($item));
